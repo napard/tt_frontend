621bbcb5a53f784616074b0439c75b150f22985b
Pablo A. Arrobbio <parrobbio@tutanota.com>
2025-07-11 10:40:31 -0300

    - Implementación completa (?) de carrito de compras.

--- js/carrito.js
+++ js/carrito.js
@@ -1,3 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    updateCartContent();
+    updateCartTotal();
+});
+
 /*const cartIncrementCount = (incr = 1) => {
     let celem = document.getElementById("carrito-count");
     celem.innerText = parseInt(celem.innerText) + incr;
@@ -15,17 +20,55 @@ const cartAddProduct = (index, count_id) => {
     /*cartIncrementCount(count);*/
     openToast("Productos agregados al carrito...");
 
-    let product = {
-        name: products_catalog[index].name,
-        count: count,
-        price: parseFloat(products_catalog[index].price.replace(",", "."))
-    };
-
     let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-    carrito.push(product);
-    localStorage.setItem("carrito", JSON.stringify(carrito));
+    let carr_index = carrito.findIndex(item => item.id === products_catalog[index].id);
+    if (carr_index != -1)
+        carrito[carr_index].count += count;
+    else {
+        carrito.push({
+            count: count,
+            id: products_catalog[index].id,
+            name: products_catalog[index].name,
+            price: products_catalog[index].price,
+            image: products_catalog[index].image
+        });
+    }
 
+    localStorage.setItem("carrito", JSON.stringify(carrito));
     updateCartCounter();
+}
+
+const cartDelProd = (id) => {
+    let cart_content = document.getElementById("cart-content");
+    let children = cart_content.children;
+    if (children.length > 0) {
+        let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+        let carr_index = carrito.findIndex(item => item.id === id);
+
+        if (carr_index != -1) {
+            carrito.splice(carr_index, 1);
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+            updateCartContent(carrito);
+            updateCartTotal();
+        }
+    }
+}
+
+const buildCartItems = (items) => {
+    let html = ``;
+
+    html = items.map(item => {
+        return `
+        <li class="cart-item">
+            <div class="cart-prod"><img src="${item.image}"></img>${item.name} <em>(x${item.count})</em></div>
+            <div class="price">
+                <div class="value">$${(parseFloat(item.price.replace(",", ".")) * item.count).toFixed(2).replace(".", ",")}</div>
+                <div class="btn btn-carrito" onclick="cartDelProd(${item.id})">Eliminar</div>
+            </div>
+        </li>`;
+    }).join("");
+
+    document.getElementById("cart-content").innerHTML = html;
 }
 
 const updateCartCounter = () => {
@@ -36,4 +79,36 @@ const updateCartCounter = () => {
     });
 
     document.getElementById("carrito-count").innerText = total_count;
+    document.getElementById("carrito-count-mob").innerText = total_count;
 }
+
+const updateCartTotal = () => {
+    let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    let total = 0;
+    carrito.map(product => {
+        total += product.count * parseFloat(product.price.replace(",", "."));
+    });
+
+    document.getElementById("cart-total").innerText = "Total: $" + total.toFixed(2);
+}
+
+const updateCartContent = () => {
+    let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    buildCartItems(carrito);
+
+    let elem = document.getElementById("cart-content");
+    if(carrito.length == 0) {
+        elem.style.fontSize = "1.5rem";
+        elem.innerText = "El carrito está vacío ¯\\_(ツ)_/¯";
+
+        elem = document.getElementById("empty-cart-btn");
+        elem.style.display = "none";
+    }
+}
+
+const emptyCart = () => {
+    localStorage.removeItem("carrito");
+    updateCartContent();
+    updateCartTotal();
+}
+
--- js/index/index.js
+++ js/index/index.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     updateCartCounter();
     /*createFakeStore();*/
     createProductsContent();
